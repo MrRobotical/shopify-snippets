82d049998837a381cb8094794fdc38f5af4ed9a7
MrRobotical <128762593+MrRobotical@users.noreply.github.com>
2024-01-20 15:39:49 -0500

    Create global.js

---
+++ display-custom-variant-data/global.js
@@ -0,0 +1,16 @@
+/*Add the following method under the onVariantChange function*/
+this.updateVariantDetails();
+
+
+/*Add the following to load and display the custom data per variant */
+updateVariantDetails() {
+
+  //load metafield data
+  const allVariantMetafieldData = JSON.parse(document.querySelector('#variant_metafield_data').textContent);
+  console.log(allVariantMetafieldData);
+      
+  //replace content in collapsible row
+  const variantDetailsTxtElement = document.querySelector('#variant-details');
+  variantDetailsTxtElement.innerHTML = allVariantMetafieldData[this.currentVariant.id];
+  
+}
